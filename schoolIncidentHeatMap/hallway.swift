1dc76e58f88cb84b47f832d6fb14f34210662c6f
Dave Borucki <david.borucki21@d155.org>
2021-02-22 08:38:22 -0600

    added hallway class

---
+++ schoolIncidentHeatMap/hallway.swift
@@ -0,0 +1,13 @@
+//
+//  hallway.swift
+//  schoolIncidentHeatMap
+//
+//  Created by Tiger Coder on 2/22/21.
+//
+
+import Foundation
+
+class Hallway {
+
+
+}
---
+++ schoolIncidentHeatMap/regionEnum.swift
@@ -0,0 +1,12 @@
+//
+//  regionEnum.swift
+//  schoolIncidentHeatMap
+//
+//  Created by Tiger Coder on 2/22/21.
+//
+
+import Foundation
+
+enum hallwayTags {
+   //case 1
+}
